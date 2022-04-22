90edbfd490e48e9576ba6fb5c75519aed31b973a
ngangavic <ngangavictor10@gmail.com>
2022-04-22 17:04:36 +0000

    clean up

--- index.php
+++ index.php
@@ -16,25 +16,6 @@ if (isset($_POST['register'])) {
         </script>
         <?php
     }
-    // $ch = curl_init();
-
-    // curl_setopt($ch, CURLOPT_URL, 'http://192.168.0.113/oauth/token.php');
-    // curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    // curl_setopt($ch, CURLOPT_POST, 1);
-    // curl_setopt($ch, CURLOPT_POSTFIELDS, "grant_type=client_credentials");
-    // curl_setopt($ch, CURLOPT_USERPWD, 'testclient' . ':' . 'testpass');
-
-    // $headers = array();
-    // $headers[] = 'Content-Type: application/x-www-form-urlencoded';
-    // curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-
-    // $result = curl_exec($ch);
-    // if (curl_errno($ch)) {
-    //     echo 'Error:' . curl_error($ch);
-    // }
-    // curl_close($ch);
-
-    // print_r($result);
 }
 
 if (isset($_POST['change-login'])) {
